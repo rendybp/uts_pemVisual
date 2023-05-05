619898380628ad9dc5759521e2756e94723c99ce
rendybp <rendibuanap@gmail.com>
2023-05-05 21:57:48 +0700

    Persiapan Tambah Hasil

--- uts/Form1.Designer.vb
+++ uts/Form1.Designer.vb
@@ -48,15 +48,15 @@ Partial Class Form1
         Me.cashbackOutput = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.Label21 = New System.Windows.Forms.Label()
-        Me.Label20 = New System.Windows.Forms.Label()
-        Me.Label19 = New System.Windows.Forms.Label()
+        Me.hasilKembalian = New System.Windows.Forms.Label()
+        Me.hasilBayar = New System.Windows.Forms.Label()
         Me.Label18 = New System.Windows.Forms.Label()
-        Me.Label17 = New System.Windows.Forms.Label()
-        Me.Label16 = New System.Windows.Forms.Label()
-        Me.Label15 = New System.Windows.Forms.Label()
-        Me.Label14 = New System.Windows.Forms.Label()
-        Me.Label13 = New System.Windows.Forms.Label()
-        Me.Label12 = New System.Windows.Forms.Label()
+        Me.hasilTotalBayar = New System.Windows.Forms.Label()
+        Me.hasilDiskon = New System.Windows.Forms.Label()
+        Me.hasilTotalHarga = New System.Windows.Forms.Label()
+        Me.hasilJam = New System.Windows.Forms.Label()
+        Me.hasilPaket = New System.Windows.Forms.Label()
+        Me.hasilUser = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -267,15 +267,15 @@ Partial Class Form1
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.Label21)
-        Me.GroupBox1.Controls.Add(Me.Label20)
-        Me.GroupBox1.Controls.Add(Me.Label19)
+        Me.GroupBox1.Controls.Add(Me.hasilKembalian)
+        Me.GroupBox1.Controls.Add(Me.hasilBayar)
         Me.GroupBox1.Controls.Add(Me.Label18)
-        Me.GroupBox1.Controls.Add(Me.Label17)
-        Me.GroupBox1.Controls.Add(Me.Label16)
-        Me.GroupBox1.Controls.Add(Me.Label15)
-        Me.GroupBox1.Controls.Add(Me.Label14)
-        Me.GroupBox1.Controls.Add(Me.Label13)
-        Me.GroupBox1.Controls.Add(Me.Label12)
+        Me.GroupBox1.Controls.Add(Me.hasilTotalBayar)
+        Me.GroupBox1.Controls.Add(Me.hasilDiskon)
+        Me.GroupBox1.Controls.Add(Me.hasilTotalHarga)
+        Me.GroupBox1.Controls.Add(Me.hasilJam)
+        Me.GroupBox1.Controls.Add(Me.hasilPaket)
+        Me.GroupBox1.Controls.Add(Me.hasilUser)
         Me.GroupBox1.Location = New System.Drawing.Point(561, 150)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(347, 436)
@@ -293,23 +293,23 @@ Partial Class Form1
         Me.Label21.TabIndex = 9
         Me.Label21.Text = "Selamat Bermain"
         '
-        'Label20
+        'hasilKembalian
         '
-        Me.Label20.AutoSize = True
-        Me.Label20.Location = New System.Drawing.Point(33, 346)
-        Me.Label20.Name = "Label20"
-        Me.Label20.Size = New System.Drawing.Size(107, 20)
-        Me.Label20.TabIndex = 8
-        Me.Label20.Text = "Kembalian     :"
+        Me.hasilKembalian.AutoSize = True
+        Me.hasilKembalian.Location = New System.Drawing.Point(33, 346)
+        Me.hasilKembalian.Name = "hasilKembalian"
+        Me.hasilKembalian.Size = New System.Drawing.Size(107, 20)
+        Me.hasilKembalian.TabIndex = 8
+        Me.hasilKembalian.Text = "Kembalian     :"
         '
-        'Label19
+        'hasilBayar
         '
-        Me.Label19.AutoSize = True
-        Me.Label19.Location = New System.Drawing.Point(34, 311)
-        Me.Label19.Name = "Label19"
-        Me.Label19.Size = New System.Drawing.Size(106, 20)
-        Me.Label19.TabIndex = 7
-        Me.Label19.Text = "Bayar             :"
+        Me.hasilBayar.AutoSize = True
+        Me.hasilBayar.Location = New System.Drawing.Point(34, 311)
+        Me.hasilBayar.Name = "hasilBayar"
+        Me.hasilBayar.Size = New System.Drawing.Size(106, 20)
+        Me.hasilBayar.TabIndex = 7
+        Me.hasilBayar.Text = "Bayar             :"
         '
         'Label18
         '
@@ -320,59 +320,59 @@ Partial Class Form1
         Me.Label18.TabIndex = 6
         Me.Label18.Text = "=============================="
         '
-        'Label17
+        'hasilTotalBayar
         '
-        Me.Label17.AutoSize = True
-        Me.Label17.Location = New System.Drawing.Point(34, 232)
-        Me.Label17.Name = "Label17"
-        Me.Label17.Size = New System.Drawing.Size(105, 20)
-        Me.Label17.TabIndex = 5
-        Me.Label17.Text = "Total Bayar   :"
+        Me.hasilTotalBayar.AutoSize = True
+        Me.hasilTotalBayar.Location = New System.Drawing.Point(34, 232)
+        Me.hasilTotalBayar.Name = "hasilTotalBayar"
+        Me.hasilTotalBayar.Size = New System.Drawing.Size(105, 20)
+        Me.hasilTotalBayar.TabIndex = 5
+        Me.hasilTotalBayar.Text = "Total Bayar   :"
         '
-        'Label16
+        'hasilDiskon
         '
-        Me.Label16.AutoSize = True
-        Me.Label16.Location = New System.Drawing.Point(33, 194)
-        Me.Label16.Name = "Label16"
-        Me.Label16.Size = New System.Drawing.Size(106, 20)
-        Me.Label16.TabIndex = 4
-        Me.Label16.Text = "Diskon           :"
+        Me.hasilDiskon.AutoSize = True
+        Me.hasilDiskon.Location = New System.Drawing.Point(33, 194)
+        Me.hasilDiskon.Name = "hasilDiskon"
+        Me.hasilDiskon.Size = New System.Drawing.Size(106, 20)
+        Me.hasilDiskon.TabIndex = 4
+        Me.hasilDiskon.Text = "Diskon           :"
         '
-        'Label15
+        'hasilTotalHarga
         '
-        Me.Label15.AutoSize = True
-        Me.Label15.Location = New System.Drawing.Point(31, 157)
-        Me.Label15.Name = "Label15"
-        Me.Label15.Size = New System.Drawing.Size(108, 20)
-        Me.Label15.TabIndex = 3
-        Me.Label15.Text = "Total Harga   :"
+        Me.hasilTotalHarga.AutoSize = True
+        Me.hasilTotalHarga.Location = New System.Drawing.Point(31, 157)
+        Me.hasilTotalHarga.Name = "hasilTotalHarga"
+        Me.hasilTotalHarga.Size = New System.Drawing.Size(108, 20)
+        Me.hasilTotalHarga.TabIndex = 3
+        Me.hasilTotalHarga.Text = "Total Harga   :"
         '
-        'Label14
+        'hasilJam
         '
-        Me.Label14.AutoSize = True
-        Me.Label14.Location = New System.Drawing.Point(32, 119)
-        Me.Label14.Name = "Label14"
-        Me.Label14.Size = New System.Drawing.Size(107, 20)
-        Me.Label14.TabIndex = 2
-        Me.Label14.Text = "Jml Jam         :"
+        Me.hasilJam.AutoSize = True
+        Me.hasilJam.Location = New System.Drawing.Point(32, 119)
+        Me.hasilJam.Name = "hasilJam"
+        Me.hasilJam.Size = New System.Drawing.Size(107, 20)
+        Me.hasilJam.TabIndex = 2
+        Me.hasilJam.Text = "Jml Jam         :"
         '
-        'Label13
+        'hasilPaket
         '
-        Me.Label13.AutoSize = True
-        Me.Label13.Location = New System.Drawing.Point(33, 82)
-        Me.Label13.Name = "Label13"
-        Me.Label13.Size = New System.Drawing.Size(106, 20)
-        Me.Label13.TabIndex = 1
-        Me.Label13.Text = "Paket             :"
+        Me.hasilPaket.AutoSize = True
+        Me.hasilPaket.Location = New System.Drawing.Point(33, 82)
+        Me.hasilPaket.Name = "hasilPaket"
+        Me.hasilPaket.Size = New System.Drawing.Size(106, 20)
+        Me.hasilPaket.TabIndex = 1
+        Me.hasilPaket.Text = "Paket             :"
         '
-        'Label12
+        'hasilUser
         '
-        Me.Label12.AutoSize = True
-        Me.Label12.Location = New System.Drawing.Point(32, 44)
-        Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(107, 20)
-        Me.Label12.TabIndex = 0
-        Me.Label12.Text = "User               :"
+        Me.hasilUser.AutoSize = True
+        Me.hasilUser.Location = New System.Drawing.Point(32, 44)
+        Me.hasilUser.Name = "hasilUser"
+        Me.hasilUser.Size = New System.Drawing.Size(107, 20)
+        Me.hasilUser.TabIndex = 0
+        Me.hasilUser.Text = "User               :"
         '
         'Form1
         '
@@ -437,15 +437,15 @@ Partial Class Form1
     Friend WithEvents Label11 As System.Windows.Forms.Label
     Friend WithEvents cashbackOutput As System.Windows.Forms.Label
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
-    Friend WithEvents Label17 As System.Windows.Forms.Label
-    Friend WithEvents Label16 As System.Windows.Forms.Label
-    Friend WithEvents Label15 As System.Windows.Forms.Label
-    Friend WithEvents Label14 As System.Windows.Forms.Label
-    Friend WithEvents Label13 As System.Windows.Forms.Label
-    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents hasilTotalBayar As System.Windows.Forms.Label
+    Friend WithEvents hasilDiskon As System.Windows.Forms.Label
+    Friend WithEvents hasilTotalHarga As System.Windows.Forms.Label
+    Friend WithEvents hasilJam As System.Windows.Forms.Label
+    Friend WithEvents hasilPaket As System.Windows.Forms.Label
+    Friend WithEvents hasilUser As System.Windows.Forms.Label
     Friend WithEvents Label21 As System.Windows.Forms.Label
-    Friend WithEvents Label20 As System.Windows.Forms.Label
-    Friend WithEvents Label19 As System.Windows.Forms.Label
+    Friend WithEvents hasilKembalian As System.Windows.Forms.Label
+    Friend WithEvents hasilBayar As System.Windows.Forms.Label
     Friend WithEvents Label18 As System.Windows.Forms.Label
 
 End Class
